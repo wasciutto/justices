639df1cb38fd7512a48192e80ec70b3b23721ca4
William <wjasciutto@gmail.com>
2024-05-05 12:15:00 -0600

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p> Justices </p>
+          <ul>
+            <li>Chief Justice</li>
+            <li>Associate Justice 1</li>
+            <li>Associate Justice 2</li>
+            <li>Associate Justice 3</li>
+          </ul>
+        </div>
+        <div>
+          <p> Slider </p>
+        </div>
       </header>
     </div>
   );
